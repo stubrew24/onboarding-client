dcd63fe3ac20b9a22bf5e45b268f40239bc4d961
stubrew24 <hello@stuartsewell.dev>
2019-06-26 23:06:49 +0100

    Initial interface

--- api.js
+++ api.js
@@ -1,38 +1,40 @@
 const BASE_URL = "http://localhost:4000/"
+
 const USERS_URL = BASE_URL + "users"
 const PROGRESS_URL = BASE_URL + "progress"
 const TASKS_URL = BASE_URL + "tasks"
 
 const list = (url) => {
-    fetch(url)
+    return fetch(url)
         .then(resp => resp.json())
-        .then(console.log)
 }
 
 const show = (url, id) => {
-    fetch(url + `/${id}`)
+    return fetch(url + `/${id}`)
         .then(resp => resp.json())
-        .then(console.log)
 }
 
 const create = (url, bodyObj) => {
-    fetch(url, {
+    return fetch(url, {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(bodyObj)
     })
+    .then(resp => resp.json())
 }
 
 const update = (url, id, bodyObj) => {
-    fetch(url + `/${id}`, {
+    return fetch(url + `/${id}`, {
         method: 'PUT',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(bodyObj)
     })
+    .then(resp => resp.json())
 }
 
 const remove = (url, id) => {
-    fetch(url + `/${id}`, {
+    return fetch(url + `/${id}`, {
         method: 'DELETE'
     })
+    .then(resp => resp.json())
 }
